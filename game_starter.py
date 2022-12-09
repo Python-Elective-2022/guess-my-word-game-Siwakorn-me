42a3c2753ec7dc46d7389252b0605f35cb8fe44d
Siwakorn-me <Siwakorn.me2024@kmids.ac.th>
2022-12-09 08:38:05 +0700

    first try

--- game_starter.py
+++ game_starter.py
@@ -1,4 +1,3 @@
-
 # -----------------------------------
 # Helper code
 # You don't need to understand this helper code,
@@ -9,6 +8,7 @@ import random
 import string
 
 WORDLIST_FILENAME = "word_list.txt"
+
 
 def load_words():
     """
@@ -27,6 +27,7 @@ def load_words():
     print(len(word_list), "words found")
     return word_list
 
+
 def choose_word(word_list):
     """
     word_list (list): list of words (strings)
@@ -35,29 +36,38 @@ def choose_word(word_list):
     """
     return random.choice(word_list)
 
+
 # end of helper code
 # -----------------------------------
 
 # Load the list of words into the variable word_list
 # so that it can be accessed from anywhere in the program
+
+
 word_list = load_words()
 
+
 def is_word_guessed(secret_word, letters_guessed):
-    '''
+    """
     secret_word: string, the word the user is guessing
     letters_guessed: list, what letters have been guessed so far
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
-    '''
+    """
     # FILL IN YOUR CODE HERE...
-    pass
+    count = 0
+    for letters in letters_guessed:
+        if letters in secret_word:
+            count += 1
+    if count == len(secret_word):
+        return True
+    else:
+        return False
 
 
-### Testcases
-# print(is_word_guessed('apple', ['a', 'e', 'i', 'k', 'p', 'r', 's']))
-# print(is_word_guessed('durian', ['h', 'a', 'c', 'd', 'i', 'm', 'n', 'r', 't', 'u']))
-# print(is_word_guessed('pineapple', []))
-
+print(is_word_guessed('apple', ['a', 'e', 'i', 'k', 'p', 'r', 's']))
+print(is_word_guessed('durian', ['h', 'a', 'c', 'd', 'i', 'm', 'n', 'r', 't', 'u']))
+print(is_word_guessed('pineapple', []))
 
 
 def get_guessed_word(secret_word, letters_guessed):
@@ -69,11 +79,9 @@ def get_guessed_word(secret_word, letters_guessed):
     '''
     # FILL IN YOUR CODE HERE...
     pass
-    
-    
-    
-      
-#Testcases
+
+
+# Testcases
 # print(get_guessed_word('apple', ['e', 'i', 'k', 'p', 'r', 's']))
 # print(get_guessed_word('durian', ['a', 'c', 'd', 'h', 'i', 'm', 'n', 'r', 't', 'u']))
 
@@ -87,10 +95,9 @@ def get_available_letters(letters_guessed):
     pass
 
 
-
-#Testcases 
+# Testcases
 # print( get_available_letters(['e', 'i', 'k', 'p', 'r', 's']) )
-  
+
 def game_loop(secret_word):
     '''
     secret_word: string, the secret word to guess.
@@ -115,11 +122,10 @@ def game_loop(secret_word):
     pass
 
 
-
-
 def main():
     secret_word = choose_word(word_list)
     game_loop(secret_word)
+
 
 # Testcases
 # you might want to pick your own
